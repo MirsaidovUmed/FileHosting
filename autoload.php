17d2bd414c2150faf0e4cffbe672e27fcbd2e101
MirsaidovUmed <umedmirsaidov61@gmail.com>
2024-04-14 09:16:51 +0500

    modofied

--- autoload.php
+++ autoload.php
@@ -4,9 +4,10 @@ spl_autoload_register(function (string $class): void {
     if (str_starts_with($class, 'App\\')) {
         $classPath = preg_replace('/^App/', '', $class);
         $classPath = str_replace('\\', DIRECTORY_SEPARATOR, $classPath);
-        $filePath = __DIR__ . $classPath . '.php';
+        $filePath = __DIR__ . DIRECTORY_SEPARATOR . 'App' . $classPath . '.php';
         if (file_exists($filePath)) {
             require_once $filePath;
         }
+        var_dump($filePath);
     }
 });

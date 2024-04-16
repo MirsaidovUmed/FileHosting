98963d4592fa48e05e7cd6f99055f037890cbe3c
MirsaidovUmed <umedmirsaidov61@gmail.com>
2024-04-16 11:30:15 +0500

    modified

--- public/index.php
+++ public/index.php
@@ -3,19 +3,16 @@
 require_once __DIR__ . "/../autoload.php";
 
 use App\Core\App;
-use App\Core\Router;
 use App\Core\Request;
 use App\Core\Response;
-use App\Core\Database;
 
-$config = require_once __DIR__ . '/../configs/database.php';
-$db = new Database($config);
+$app = new App();
 
-$app = new App($db);
-
-$router = new Router($db);
+$router = $app->getRouter();
 
 $request = new Request();
-$response = $router->processRequest($request);
+$url = $request->getRoute();
+$method = $request->getMethod();
+$response = $router->dispatch($url, $method);
 
 $response->send();
